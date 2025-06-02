01b8aa0d58fae9891ea86fbc7b8364362d6db75c
Aditya Shishu <40406705+adityashishu147@users.noreply.github.com>
2025-06-02 19:25:06 +0530

    Create nestedif.py

---
+++ nestedif.py
@@ -0,0 +1,15 @@
+
+
+x = int(input("Enter your first number:--"))
+y = int(input("Enter your first number:--"))
+
+
+
+if x > y:
+    print( x, "is greater number than " , y)
+    if x > y:
+        print ( x, "is greater number than 15" )
+    else:
+        print ( x, "is not greater number than 15 " , y)
+else:
+    print ( x, "is not greater number than " , y)
