c8987a331bb267c41bddbfb8a0024e9ec534b581
Aditya Shishu <40406705+adityashishu147@users.noreply.github.com>
2025-06-02 19:15:04 +0530

    Create control_statement.py

---
+++ control_statement.py
@@ -0,0 +1,11 @@
+name = str(input("Enter your name:- "))
+age = int(input("Enter your age:-- "))
+
+
+
+if age >= 18:
+    print (name,"Your are adult" ,age)
+elif age < 18 :
+    print ("you are not adult ")
+else:
+    print ("you are child " , name)
