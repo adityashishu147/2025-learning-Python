727a2b48b5a9f9db3415790006f2e4bc8759974d
Aditya Shishu <40406705+adityashishu147@users.noreply.github.com>
2025-06-03 19:05:31 +0530

    request.py

---
+++ python/request.py
@@ -0,0 +1,9 @@
+
+import requests
+
+response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
+
+if response.status_code == 200:
+    print (response.json())
+else:
+    print(f'Error : {response.status_code}')
